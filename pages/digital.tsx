642be505eb16c3c1f38e9e1766c9092af554f14d
unknown <mscho88@gmail.com>
2023-05-22 23:57:53 -0400

    Added default pages for printing and digital as well as the menubar

---
+++ pages/digital.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuBar from '../components/MenuBar';
+import Digital from '../components/Digital';
+
+const DigitalPage = () => {
+  return (
+    <div>
+      <MenuBar />
+      <Digital />
+    </div>
+  );
+};
+
+export default DigitalPage;
