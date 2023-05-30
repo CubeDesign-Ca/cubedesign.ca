20add81bbc07af78dff23d082ef5114390ccd0a7
AustynYou <austyn.you@gmail.com>
2023-05-29 21:15:38 -0700

    :bookmark: reusable contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,14 @@
 "use client";
 import React, { useState, useCallback, ChangeEvent } from "react";
-import { Box, TextField, MenuItem } from "@/lib/mui";
+import {
+  Box,
+  TextField,
+  MenuItem,
+  Typography,
+  Button,
+  Grid,
+} from "@mui/material";
+import Container from '@mui/material/Container';
 
 interface Form {
   firstName: string;
@@ -18,8 +26,8 @@ export const formDefaultValue = {
 
 const currencies = [
   {
-    value: 'placeholder',
-    label: 'Select a service you would like to request'
+    value: "placeholder",
+    label: "Select a service you would like to request",
   },
   {
     value: "USD",
@@ -39,7 +47,11 @@ const currencies = [
   },
 ];
 
-const Contact = () => {
+interface ContactProps {
+  url: string;
+}
+
+const Contact = ({ url }: ContactProps) => {
   const [form, setForm] = useState<Form>(formDefaultValue);
 
   const handleChange = useCallback(
@@ -51,98 +63,112 @@ const Contact = () => {
   );
 
   return (
-    <div>
-    
-<Typography variant="h1" component="h2">
-  h1. Heading
-</Typography>;
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <img src={url} alt="contact-img" />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h4" component="h3">
+            Contact Us
+          </Typography>
 
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "50ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Select"
-          defaultValue="placeholder"
-          helperText="Please select your currency"
-          variant="standard"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="firstName"
-          name="firstName"
-          label="First Name"
-          value={form.firstName}
-          onChange={handleChange}
-          variant="standard"
-        />
-        <TextField
-          id="lastName"
-          name="lastName"
-          label="Last Name"
-          value={form.lastName}
-          onChange={handleChange}
-          variant="standard"
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "50ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="email"
-          name="email"
-          type="email"
-          label="Email Address"
-          value={form.email}
-          onChange={handleChange}
-          variant="standard"
-        />
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "50ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="message"
-          name="message"
-          label="Message (optional)"
-          value={form.message}
-          onChange={handleChange}
-          multiline
-          rows={4}
-        />
-      </Box>
-    </div>
+          <Typography variant="body1" component="p">
+            Ready to take your offline presence to the next level? 123-456-7890
+          </Typography>
+
+          <Box
+            component="div"
+            sx={{
+              "& .MuiTextField-root": { my: 1, width: "100%" },
+            }}
+          >
+            <TextField
+              id="standard-select-currency"
+              select
+              label="Select"
+              defaultValue="placeholder"
+              helperText="Please select your currency"
+              variant="standard"
+            >
+              {currencies.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              display: "flex",
+              "flex-wrap": "nowrap",
+              columnGap: "1vh",
+              "& > :not(style)": { my: 1, width: "49%" },
+            }}
+          >
+            <TextField
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              value={form.firstName}
+              onChange={handleChange}
+              variant="standard"
+            />
+            <TextField
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              value={form.lastName}
+              onChange={handleChange}
+              variant="standard"
+            />
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              "& > :not(style)": { my: 1, width: "100%" },
+            }}
+          >
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email Address"
+              value={form.email}
+              onChange={handleChange}
+              variant="standard"
+            />
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              "& > :not(style)": { my: 1, width: "100%" },
+            }}
+          >
+            <TextField
+              id="message"
+              name="message"
+              label="Message (optional)"
+              value={form.message}
+              onChange={handleChange}
+              multiline
+              rows={4}
+            />
+          </Box>
+          <Box
+            component="div"
+            sx={{
+              "& > :not(style)": { my: 1, width: "100%" },
+            }}
+          >
+            <Button sx={{ "bgcolor": "#D9D9D9" }} variant="contained">
+              Submit
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
