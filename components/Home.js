51415a89f3f852f02dd94d3ae6a914550205c777
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2023-07-27 23:55:47 -0400

    Squashed commit of the following:
    commit 0d94ccc248e55367898a2e8ec2522d94254532ce
    Merge: c2168a7 4a91717
    Author: Heejeong Seo <105368000+abcd1802@users.noreply.github.com>
    Date:   Thu Jul 27 22:40:15 2023 -0400
    
        Merge pull request #16 from CubeDesign-Ca/summer
    
        Summer
    
    commit 4a9171735bd360999ad9052f34a45b420ad50c4f
    Author: abcd1802 <sseo18@myseneca.ca>
    Date:   Thu Jul 27 21:47:45 2023 -0400
    
        Fixed width
    
    commit a089c77dea6e3c858b29b3a4f913339504dd9306
    Author: abcd1802 <sseo18@myseneca.ca>
    Date:   Thu Jul 27 19:37:33 2023 -0400
    
        Fixed service and bv container
    
    commit c2168a77695cc25cfea3654eb2c1efde530bcbbc
    Merge: fb47e9c 735ea00
    Author: Luke Heo <71468858+lukeheo8592@users.noreply.github.com>
    Date:   Thu Jul 27 20:37:34 2023 -0400
    
        Merge pull request #15 from CubeDesign-Ca/geunhyeok
    
        Geunhyeok

--- components/Home.js
+++ components/Home.js
@@ -1,72 +1,81 @@
-import React from 'react';
-import styles from '../styles/Homepage.module.css';
-import Image from 'next/image';
+
+import styles from "../app/homePage.module.css"
+
+import { React, useState } from "react";
 
 const Home = () => {
+  const [isCard1Hovered, setIsCard1Hovered] = useState(false);
+  const [isCard2Hovered, setIsCard2Hovered] = useState(false);
+
   return (
     <>
-      <figure className={styles.imgContainer}>
-        <Image className={styles.img} src="/images/Home-main_banner(2000x1090_CFE2EB).jpg" alt="Main Banner" width="100" height="100"/>
+      {/* main img */}
+      <div className={styles.imgContainer}>
         <div className={styles.mainTitle}>
           <figcaption>Cube Design & Communication</figcaption>
           <figcaption>makes Poster Design</figcaption>
         </div>
-      </figure>
-      
-      <div className={styles.mainContainer}>
-        <section className={styles.section}>
-          <div className={styles.container}>
-            <figure className={styles.card}>
-              <div>
-              <Image class={styles.img} src="/images/main-service-printing.jpg" alt="Printing Image" width="100" height="100"/>
-              <Image class={styles.imgHover} src="/images/main-service-printing-click.jpg" alt="Printing Image Clicked" width="100" height="100"/>
+      </div>
+      <div className={styles.container}>
+        {/* printing & digital */}
+        <div className={`${styles.mainContainer}`}>
+          <div className={`${styles.service}`}>
+            {/* printing */}
+            <div
+              className={`${styles.card} ${styles.card1}`}
+              style={{ backgroundImage: isCard1Hovered ? "url('/images/main-service-printing.jpg')" : "url('/images/main-service-printing-click.jpg')" }}
+              onMouseEnter={() => setIsCard1Hovered(true)}
+              onMouseLeave={() => setIsCard1Hovered(false)}
+            >
+              <div className={styles.serviceContent}>
+                <h3 className={styles.serviceTitle}>Printing</h3>
+                <p className={styles.serviceDesc}>
+                  Maximize your visual impact with our professional printing and installation services, including way finding and promotional products.
+                </p>
               </div>
-              <div className={styles.textOverlay}>
-                <h3 className={styles.divTitle}>Printing</h3>
-                <p>Maximize your visual impact with our professional printing and installation services, including way finding and promotional products.</p>
-              </div>
-            </figure>
-            <figure className={styles.card}>
-              <Image class={styles.img} src="/images/main-service-digital.jpg" alt="Digital Image" width="100" height="100"/>
-              <Image class={styles.imgHover} src="/images/main-service-digital-click.jpg" alt="Digital Image Clicked" width="100" height="100"/>
-              <div className={styles.textOverlay}>
-                <h3 className={styles.divTitle}>Digital</h3>
-                <p>Establish your online presence with our full-service solutions tailored to enhance your business.</p>
-              </div>
-            </figure>
-          </div>
-        </section>
+            </div> 
 
-        <section className={styles.section}>
-          <h3 className={styles.containerTitle}>Brand Value</h3>
-          <div className={styles.container}>
-            <div className={styles.brandValueCard}>
-              <h4 className={styles.divTitle}>One-Stop Solution</h4>
-              <p>
-                One place for all your needs full-service offerings spanning offline and online realms.
-              </p>
-            </div>
-            <div className={styles.brandValueCard}>
-              <h4 className={styles.divTitle}>Trustworthy & Professional Service</h4>
-              <p>
-                Reliable, professional services tailored to your needs, delivered promptly to elevate your business.
-              </p>
-            </div>
-            <div className={styles.brandValueCard}>
-              <h4 className={styles.divTitle}>After-Sales Supports</h4>
-              <p>
-                Robust after-sales support, ensuring our commitment to standing by services post-delivery.
-              </p>
+            <div
+              className={`${styles.card} ${styles.card2}`}
+              style={{ backgroundImage: isCard2Hovered ? "url('/images/main-service-digital.jpg')" : "url('/images/main-service-digital-click.jpg')" }}
+              onMouseEnter={() => setIsCard2Hovered(true)}
+              onMouseLeave={() => setIsCard2Hovered(false)}
+            >
+              <div className={styles.serviceContent}>
+                <h3 className={styles.serviceTitle}>Digital</h3>
+                <p className={styles.serviceDesc}>
+                  Establish your online presence with our full-service solutions tailored to enhance your business.
+                </p>
+              </div> 
             </div>
           </div>
-        </section>
+          <div className={styles.section}>
+            <h3 className={styles.bvContainerTitle}>Brand Value</h3>
+            <div className={styles.bvContainer}>
+              <div className={styles.bvCard}>
+                <h4 className={styles.bvTitle}>One-Stop Solution</h4>
+                <p className={styles.bvDesc}>
+                  One place for all your needs full-service offerings spanning offline and online realms.
+                </p>
+              </div>
+              <div className={styles.bvCard}>
+                <h4 className={styles.bvTitle}>Trustworthy &<br /> Professional Service</h4>
+                <p className={styles.bvDesc}>
+                  Reliable, professional services tailored to your needs, delivered promptly to elevate your business.
+                </p>
+              </div>
+              <div className={styles.bvCard}>
+                <h4 className={styles.bvTitle}>After-Sales Supports</h4>
+                <p className={styles.bvDesc}>
+                  Robust after-sales support, ensuring our commitment to standing by services post-delivery.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
 export default Home;
-
-
-
-
