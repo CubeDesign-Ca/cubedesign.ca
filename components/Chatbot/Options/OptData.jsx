52062163ec58ce3c6a1bc95f47630a49eb6dcaaf
JISOO LEE <jisoolee@ca.ibm.com>
2023-09-05 21:27:57 -0400

    add options in optdata

--- components/Chatbot/Options/OptData.jsx
+++ components/Chatbot/Options/OptData.jsx
@@ -49,23 +49,93 @@ const OptData = {
   printing1a : [
     {
       text: "Large Format Printing",
-      id: "p1a1"
+      id: "p1a"
     },
     { 
       text: "Digital Printing Service",
-      id: "p1a2" 
+      id: "p1b" 
     },
     { 
       text: "Installation Service",
-      id: "p1a3" 
+      id: "p1c" 
     },
     { 
       text: "All of the above",
-      id: "p1a4"
+      id: "p1d"
     },
     { 
       text: "Back to previous menu", 
-      id: "p1a5" 
+      id: "p1e" 
+    },
+  ],
+  printing2a : [
+    {
+      text: "Directional Signage Design",
+      id: "p2a"
+    },
+    { 
+      text: "Back to the previous menu",
+      id: "p2b" 
+    },
+  ],
+  printing3a : [
+    {
+      text: "Promotional Product & Merchandise",
+      id: "p3a"
+    },
+    { 
+      text: "Back to the previous menu",
+      id: "p3b" 
+    },
+  ],
+  printing4a : [
+    {
+      text: "Yes",
+      id: "p4a"
+    },
+    { 
+      text: "No",
+      id: "p4b"
+    },
+  ],
+  printing1a1 : [
+    {
+      text: "Yes",
+      id: "p1a1"
+    },
+    { 
+      text: "No",
+      id: "p1a2" 
+    },
+  ],
+  printing1b1 : [
+    {
+      text: "Yes",
+      id: "p1b1"
+    },
+    { 
+      text: "No",
+      id: "p1b2" 
+    },
+  ],
+  printing1c1 : [
+    {
+      text: "Yes",
+      id: "p1c1"
+    },
+    { 
+      text: "No",
+      id: "p1c2" 
+    },
+  ],
+  printing1d1 : [
+    {
+      text: "Yes",
+      id: "p1d1"
+    },
+    { 
+      text: "No",
+      id: "p1d2" 
     },
   ],
     
