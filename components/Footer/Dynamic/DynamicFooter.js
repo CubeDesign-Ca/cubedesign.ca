f18d273d41a4f5902cc84c003c99cff3270d91f9
unknown <mscho88@gmail.com>
2023-10-17 00:57:20 -0400

    Fixed contact us button on footer

--- components/Footer/Dynamic/DynamicFooter.js
+++ components/Footer/Dynamic/DynamicFooter.js
@@ -8,6 +8,7 @@ const DynamicFooter = () => {
   const { t } = useTranslation('common')
   const [prev, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,10 +36,21 @@ const DynamicFooter = () => {
         <div className="flex items-center">
           <PhoneIcon className="w-6 h-6 mr-[5px]" />
           <JsonData jsonKey="phone" /><p className="text-lg mx-[40px]"> or</p>
-          <Link
-            // className="bg-white text-cube_blue font-semibold px-[30px] py-[8px] text-[20px]"
-            className="bg-white text-cube_blue font-semibold px-[30px] py-[8px] text-[20px] border-b-5 border-solid border-cube_red"
-            href="/contact">{t('contactus')}</Link>
+          <div>
+            <Link
+              // className="bg-white text-cube_blue font-semibold px-[30px] py-[8px] text-[20px]"
+              className="bg-white text-cube_blue font-semibold px-[30px] py-[8px] text-[20px] border-b-5 border-solid border-cube_red"
+              href="/contact"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              {t('contactus')}
+            </Link>
+            {isHovered && <div className="absolute w-[169px] bottom-[12px] h-[5px] bg-cube_red z-2">
+
+            </div>}
+            
+          </div>
         </div>
       </div>
     </footer>
